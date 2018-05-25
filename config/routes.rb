5c70ac17bd6e447a92f866be7eb2b5551d9ffee6
Anthony McShane <a89th@hotmail.com>
2018-05-25 11:27:31 +0100

    doses controller complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "cocktails", to: "cocktails#index"
+  # get "cocktails/:id", to: "cocktails#show"
+
+  #  get "cocktails/new", to: "cocktails#new"
+  # post "cocktails", to: "cocktails#create"
+  resources :cocktails, only: [:create, :index, :new, :show] do
+    resources :doses, only: [:create, :new]
+  end
+  resources :doses, only: [:destroy]
 end
